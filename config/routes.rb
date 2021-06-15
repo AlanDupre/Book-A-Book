112414ee322567c55a9d9d728d6517f4525e9a9b
Maxence Laplante <maxence.laplante@gmail.com>
2021-06-15 17:46:23 +0200

    Routes Transactions

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :books
+  resources :books do
+    resources :transactions, only: [:new, :create]
+    member do
+      patch  "/validate", to: "transctions#validate"
+      patch  "/decline", to: "transactions#decline"
+    end
+  end
 end
